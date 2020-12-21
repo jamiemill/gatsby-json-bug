f5d805c62c7a10cc6c2e1009f8f55d355398dafa
Jamie Mill <jamiermill@gmail.com>
2020-12-21 20:53:43 +0000

    Make all sets have same images; problem persists

--- generate-sets.js
+++ generate-sets.js
@@ -9,7 +9,30 @@ const setsProcessed = sets.map(set => ({
         "month": 12,
         "period": 1,
         "day": 1
-    }
+    },
+    "images": [
+        {
+          "filename": "145--1--KILLING+MACHINE+Georgina.jpg",
+          "sourcePath": "145--1--KILLING+MACHINE+Georgina.jpg",
+          "source": "squarespace-images-fashion",
+          "order": 1,
+          "isCover": false
+        },
+        {
+          "filename": "145--2--london+fashion+Georgina+.jpg",
+          "sourcePath": "145--2--london+fashion+Georgina+.jpg",
+          "source": "squarespace-images-fashion",
+          "order": 2,
+          "isCover": false
+        },
+        {
+          "filename": "145--3--RAYGUN+Georgina.jpg",
+          "sourcePath": "145--3--RAYGUN+Georgina.jpg",
+          "source": "squarespace-images-fashion",
+          "order": 3,
+          "isCover": false
+        }
+      ]
 }));
 
 fs.writeFileSync("./src/data/sets.json", JSON.stringify(setsProcessed, null, 2))
